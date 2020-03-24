72b76d565319982806a64310154d4c06e3b52c93
Pivain <louis--76@hotmail.fr>
2020-03-24 15:09:13 +0100

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+// recuperer tous les éléments DOM dont on a besoin
+
+// par exemple document.getElementById("todo")
+
+
+// **************************************
+
+
+// Ici déclarer les variables dont on a besoin (la liste des todos par exemple)
+
+
+
+// ***************************************
+
+
+// écrire les fonction du todo
+
+// ajouter un todo
+
+// mettre a jour un todo (fait, pas fait)
+
+// supprimer un todo
+
+// afficher les todos (inserer les éléments HTML dans la page)
+
+
+// ***************************************
+
+// Fonction pour le stockage, avec localStorage
+
+// aller chercher les todos dans la mémoire
+
+// mettre à jour le localStorage
+
+// vider le le localStorage
+
+// ***************************************
+
+// Ajouter les écouteurs d'événements
